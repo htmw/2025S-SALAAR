b51d80a97f3306d6a7a430b1e9e948b934f92554
Paul Morales <paulmorales@Pauls-MBP.lan>
2025-02-13 21:46:34 -0500

    completed model predicition function - added prediction of the model - returning results

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ def model_prediction(test_image) :
     model = tf.keras.models.load_model('trained_model.keras')
     image = tf.keras.preprocessing.image.load_img(test_image, target_size = (128, 128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    #Convert single image to a batch
+    prediction = model.predict(input_arr)
+    result_index = np.argmax(prediction)
+    return result_index
+
+#added prediction to the model, returning results, completed model prediction function
+
     
