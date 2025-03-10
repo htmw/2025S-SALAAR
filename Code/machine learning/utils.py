64b5b6b7b1313c507abe4676e317f77573684e76
ManuKot <mm99806n@pace.edu>
2025-03-10 16:59:52 -0400

    Create utils.py

---
+++ Code/machine learning/utils.py
@@ -0,0 +1,71 @@
+import os
+import torch
+import torch.nn.functional as F
+import numpy as np
+from config import Config
+
+class AverageMeter:
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+        
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def accuracy(output, target, topk=(1,)):
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+        
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+        
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+def save_checkpoint(state, is_best, filename=None):
+    if filename is None:
+        filename = Config.CHECKPOINT_PATH
+    
+    torch.save(state, filename)
+    
+    if is_best:
+        best_filename = os.path.join(
+            Config.MODELS_DIR,
+            f"best_model_{Config.RUN_ID}.pth"
+        )
+        torch.save(state, best_filename)
+        print(f"Saved best model to {best_filename}")
+
+def mixup_data(x, y, alpha=1.0):
+    if alpha > 0:
+        lam = np.random.beta(alpha, alpha)
+    else:
+        lam = 1
+
+    batch_size = x.size()[0]
+    index = torch.randperm(batch_size).to(x.device)
+
+    mixed_x = lam * x + (1 - lam) * x[index, :]
+    y_a, y_b = y, y[index]
+    
+    return mixed_x, y_a, y_b, lam
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
