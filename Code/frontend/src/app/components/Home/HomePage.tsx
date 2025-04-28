b432dd05b0f3252d1aae59c33b8026871161e410
Nikithaarpula <nikithaarpula97@gmail.com>
2025-04-28 14:28:05 -0400

    Create HomePage.tsx

---
+++ Code/frontend/src/app/components/Home/HomePage.tsx
@@ -0,0 +1,21 @@
+import HeroSection from './HeroSection';
+import RecentScansSection from './RecentScansSection';
+import OrchardHealthSection from './OrchardHealthSection';
+import QuickActions from './QuickActions';
+import { ScanHistory } from '../../types';
+
+interface HomePageProps {
+  scanHistory: ScanHistory[];
+  setActiveTab: (tab: 'home' | 'detection' | 'history') => void;
+}
+
+export default function HomePage({ scanHistory, setActiveTab }: HomePageProps) {
+  return (
+    <>
+      <HeroSection setActiveTab={setActiveTab} />
+      <RecentScansSection scanHistory={scanHistory} setActiveTab={setActiveTab} />
+      <OrchardHealthSection />
+      <QuickActions setActiveTab={setActiveTab} />
+    </>
+  );
+}
